657a22f58b2a6039b9d4024c80790f3c87e4adfe
federicodececco <federicodececco1@gmail.com>
2024-12-06 16:10:45 +0100

    add:button

--- script.js
+++ script.js
@@ -8,11 +8,15 @@ function fiatLux() {
     let validation = trigger.classList.contains("check");
     if (validation) {
         trigger.classList.remove("check")
+        trigger.classList.remove("check")
         trigger.src = './img/yellow_lamp.png'
+        document.getElementById("btn-primary").innerHTML = "Spegni";
     }
     else {
         trigger.classList.add("check")
+        trigger.classList.remove("checkers")
         trigger.src = './img/white_lamp.png'
+        document.getElementById("btn-primary").innerHTML = "Accendi";
     }
 
 
