2d84e5ce3389ba0cc051ba7650fb094450dd737b
federicodececco <federicodececco1@gmail.com>
2024-12-06 16:24:47 +0100

    add: bg

--- script.js
+++ script.js
@@ -6,15 +6,20 @@
 function fiatLux() {
     let trigger = document.getElementById('ima')
     let validation = trigger.classList.contains("check");
+    let bd = document.body;
     if (validation) {
         trigger.classList.remove("check")
-        trigger.classList.remove("check")
+        trigger.classList.add("checkers")
+        bd.classList.remove("dark")
+        bd.classList.add("light")
         trigger.src = './img/yellow_lamp.png'
         document.getElementById("btn-primary").innerHTML = "Spegni";
     }
     else {
         trigger.classList.add("check")
         trigger.classList.remove("checkers")
+        bd.classList.add("dark")
+        bd.classList.remove("light")
         trigger.src = './img/white_lamp.png'
         document.getElementById("btn-primary").innerHTML = "Accendi";
     }
